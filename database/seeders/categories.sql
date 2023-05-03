7ce39a808e3402496cadeb2cd442f1f6e0370b69
marcmahinay <marc.mahinay@gmail.com>
2023-05-03 17:24:51 +0800

    update seeder

---
+++ database/seeders/categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO `categories` (`code`,`description`,`created_at`, `updated_at`) VALUES
+('FCS','Food/Catering Services',now(),now()),
+('IP','Infrastructure Projects',now(),now()),
+('MMS','Medicines and Medical Supplies',now(),now()),
+('OGS','Other Goods and Services',now(),now()),
+('OSE','Office Supplies and Equipments',now(),now());
---
+++ database/seeders/uoms.sql
@@ -0,0 +1,13 @@
+INSERT INTO `uoms` (`name`,`description`,`created_at`, `updated_at`) VALUES
+('pc','piece',now(),now()),
+('bottle','bottle',now(),now()),
+('ream','ream',now(),now()),
+('can','can',now(),now()),
+('pack','pack',now(),now()),
+('box','box',now(),now()),
+('unit','unit',now(),now()),
+('set','set',now(),now()),
+('cart','cartridge',now(),now()),
+('pad','pad',now(),now()),
+('tab','tablet',now(),now()),
+('roll','roll',now(),now());
