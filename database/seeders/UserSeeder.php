dd66c48d70de01a6990a52872fc4adc4a2110f5b
marcmahinay <marc.mahinay@gmail.com>
2023-05-04 06:00:29 +0800

    added seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,15 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class OfficeSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::unprepared(file_get_contents(__DIR__ . '/offices.sql'));
+        DB::unprepared(file_get_contents(__DIR__ . '/users.sql'));
     }
 }
---
+++ database/seeders/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users
+   (`id`, `name`, `email`, `email_verified_at`, `password`, `avatar`, `remember_token`, `created_at`, `updated_at`)
+VALUES
+   (1, 'admin', 'admin@gmail.com', NULL, '$2y$10$40XI8RjJja2MK627/SIWJ.5N3bC9G.OwCDggtoyvuTSVvzXKnzRUG', NULL, NULL, NOW(), NOW());
