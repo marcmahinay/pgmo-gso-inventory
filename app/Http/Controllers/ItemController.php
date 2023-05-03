84274989c1d1d4b1bfb0e25db836f61f56e7240d
marcmahinay <marc.mahinay@gmail.com>
2023-05-03 17:21:02 +0800

    added index()

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -12,7 +12,16 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::select('items.id', 'stock_no', 'items.description', 'uoms.name as uom_name', 'categories.code as cat_code', 'categories.description as cat_desc','reorder_point')
+                    ->join('uoms', 'uom_id', '=', 'uoms.id')
+                    ->join('categories','category_id','=','categories.id')
+                    ->orderBy('description')
+                    ->paginate(10);
+                    //->get();
+        $data = compact(['items']);
+
+        //dd($items);
+        return view('dashlite.item.index', $data);
     }
 
     /**
