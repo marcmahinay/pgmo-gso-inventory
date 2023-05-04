974f6aae1740114c4b367cd3363875f140433d10
marcmahinay <marc.mahinay@gmail.com>
2023-05-04 16:29:40 +0800

    change query on index()

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -12,10 +12,12 @@ class ItemController extends Controller
      */
     public function index()
     {
-        $items = Item::select('items.id', 'stock_no', 'items.description', 'uoms.name as uom_name', 'categories.code as cat_code', 'categories.description as cat_desc','reorder_point')
+        /* $items = Item::select('items.id', 'stock_no', 'items.description', 'uoms.name as uom_name', 'categories.code as cat_code', 'categories.description as cat_desc','reorder_point')
                     ->join('uoms', 'uom_id', '=', 'uoms.id')
                     ->join('categories','category_id','=','categories.id')
                     ->orderBy('description')
+                    ->paginate(10); */
+        $items = Item::orderBy('description')
                     ->paginate(10);
                     //->get();
         $data = compact(['items']);
